39228bb9248137df645fcc15eb395590b9d814bc
Gabriel <gabrielwest621@gmail.com>
2019-07-10 21:05:31 -0700

    fixed typo

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import Logo from "../components/Logo"
+import Logo from "../components/logo"
 
 import Header from "./header"
 import "./layout.css"
