80b4a045f8363fb6dd4f2d128354945aa403bc83
gabrielawest <gabrielwest.dev@gmail.com>
2019-08-03 11:43:39 -0400

    added under construction page

---
+++ src/pages/underConstruction.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ConstructionPage = () => (
+  <Layout>
+    <SEO title="UNDER CONSTRUCTION" />
+    <h1>THIS WILL BE OUR SERVICES</h1>
+    <p>We will sell lots of things and offer even more!</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default ConstructionPage
