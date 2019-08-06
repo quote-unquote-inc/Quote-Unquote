da6e7d46ea5eeda2e7226c67b9e1f8fd43a0b95d
Gabriel <gabrielwest621@gmail.com>
2019-08-06 19:50:30 -0400

    visual improvements to contact page

--- src/components/contactMultiback.js
+++ src/components/contactMultiback.js
@@ -6,7 +6,7 @@ import BackgroundImage from "gatsby-background-image"
 
 const H1 = styled.h1`
    font-size: 3.25rem;
-   color: black !important;
+   color: white !important;
 `
 
 const MultiBackground = ({ children, className }) => {
@@ -49,179 +49,184 @@ const MultiBackground = ({ children, className }) => {
          <StyledInnerWrapper>
             <div
                css={{
-                  color: "black",
+                  color: "white",
                   width: "80vw",
+                  backgroundColor: "rgba(0, 0, 0, 0.5)",
+                  padding: "13px",
+                  borderRadius: "13px",
                }}
             >
-               <H1
-                  css={{
-                     fontSize: "3.25rem",
-                     "@media(max-width: 480px)": {
-                        fontSize: "3rem",
-                     },
-                  }}
-               >
-                  Contact us directly:
-               </H1>
-               <p
-                  css={{
-                     fontSize: "1.5rem",
-                     lineHeight: "36px",
-                     "@media(max-width: 480px)": {
-                        fontSize: "1.3rem",
-                     },
-                  }}
-               >
-                  Call us @{" "}
-                  <a
+               <div css={{}}>
+                  <H1
                      css={{
-                        color: "black",
-                        ":hover": {
-                           color: "#cb1741",
+                        fontSize: "3.25rem",
+                        "@media(max-width: 480px)": {
+                           fontSize: "3rem",
                         },
                      }}
-                     href="tel:+15055017700"
                   >
-                     (505) 501-7700
-                  </a>
-                  <br />
-                  Reach us by mail @ PO Box 26206, Albuquerque, NM 87107
-                  <br />
-                  Or come see us @{" "}
-                  <a
+                     Contact us directly:
+                  </H1>
+                  <p
                      css={{
-                        color: "black",
-                        ":hover": {
-                           color: "#cb1741",
+                        fontSize: "1.5rem",
+                        lineHeight: "36px",
+                        "@media(max-width: 480px)": {
+                           fontSize: "1.3rem",
                         },
                      }}
-                     href="http://maps.google.com/?q=5005 4th St. NW Ste. 102, Albuquerque NM 87107"
-                     target="_blank"
-                     rel="noopener noreferrer"
                   >
-                     5005 4th St. NW Ste. 102, Albuquerque NM 87107
-                  </a>
-               </p>
-               <H1
-                  css={{
-                     fontSize: "3.25rem",
-                     "@media(max-width: 480px)": {
-                        fontSize: "3rem",
-                     },
-                  }}
-               >
-                  Or send us a message:
-               </H1>
-            </div>
+                     Call us @{" "}
+                     <a
+                        css={{
+                           color: "white",
+                           ":hover": {
+                              color: "darkred",
+                           },
+                        }}
+                        href="tel:+15055017700"
+                     >
+                        (505) 501-7700
+                     </a>
+                     <br />
+                     Reach us by mail @ PO Box 26206, Albuquerque, NM 87107
+                     <br />
+                     Or come see us @{" "}
+                     <a
+                        css={{
+                           color: "white",
+                           ":hover": {
+                              color: "darkred",
+                           },
+                        }}
+                        href="http://maps.google.com/?q=5005 4th St. NW Ste. 102, Albuquerque NM 87107"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                     >
+                        5005 4th St. NW Ste. 102, Albuquerque NM 87107
+                     </a>
+                  </p>
+                  <H1
+                     css={{
+                        fontSize: "3.25rem",
+                        "@media(max-width: 480px)": {
+                           fontSize: "3rem",
+                        },
+                     }}
+                  >
+                     Or send us a message:
+                  </H1>
+               </div>
 
-            <div className="form">
-               <form
-                  method="post"
-                  action="#"
-                  netlify-honeypot="bot-field"
-                  data-netlify="true"
-                  name="contact"
-                  css={{
-                     textAlign: "center",
-                     margin: "0",
-                  }}
-               >
-                  <input type="hidden" name="form-name" value="contact" />
-                  <input type="hidden" name="bot-field" />
-                  <input
-                     type="text"
-                     name="name"
-                     id="name"
-                     aria-label="Full Name"
-                     placeholder="Full Name"
+               <div className="form">
+                  <form
+                     method="post"
+                     action="#"
+                     netlify-honeypot="bot-field"
+                     data-netlify="true"
+                     name="contact"
                      css={{
                         textAlign: "center",
-                        width: "70vw",
-                        margin: "13px 13%",
-                        background: "transparent",
-                        color: "black",
-                        borderStyle: "groove",
-                        borderColor: "#cb1741",
-                        borderLeft: "0px",
-                        borderRight: "0px",
-                        borderTop: "0px",
-                        borderRadius: "9px",
+                        margin: "0",
                      }}
-                     required
-                  />
-                  <input
-                     type="text"
-                     name="email"
-                     id="email"
-                     aria-label="Email"
-                     placeholder="email@domain.com"
-                     css={{
-                        textAlign: "center",
-                        width: "70vw",
-                        margin: "13px 13%",
-                        background: "transparent",
-                        color: "black",
-                        borderStyle: "groove",
-                        borderColor: "#cb1741",
-                        borderLeft: "0px",
-                        borderRight: "0px",
-                        borderTop: "0px",
-                        borderRadius: "9px",
-                     }}
-                     required
-                  />
-                  <input
-                     type="text"
-                     name="phone"
-                     id="phone"
-                     aria-label="Phone Number"
-                     placeholder="Phone (### ### ####)"
-                     css={{
-                        textAlign: "center",
-                        width: "70vw",
-                        margin: "13px 13%",
-                        background: "transparent",
-                        color: "black",
-                        borderStyle: "groove",
-                        borderColor: "#cb1741",
-                        borderLeft: "0px",
-                        borderRight: "0px",
-                        borderTop: "0px",
-                        borderRadius: "9px",
-                     }}
-                  />
-                  <textarea
-                     name="message"
-                     id="message"
-                     rows="4"
-                     aria-label="Message"
-                     placeholder="Type Your Message Here"
-                     required
-                     css={{
-                        textAlign: "center",
-                        width: "70vw",
-                        margin: "13px 13%",
-                        background: "transparent",
-                        color: "black",
-                        borderStyle: "groove",
-                        borderColor: "#cb1741",
-                        borderRadius: "9px",
-                     }}
-                  />
-                  <input
-                     type="submit"
-                     value="Send Message"
-                     css={{
-                        margin: "0 20%",
-                        borderRadius: "13px",
-                        background: "transparent",
-                        color: "black",
-                        ":active": {
-                           backgroundColor: "#BE0405",
-                        },
-                     }}
-                  />
-               </form>
+                  >
+                     <input type="hidden" name="form-name" value="contact" />
+                     <input type="hidden" name="bot-field" />
+                     <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        aria-label="Full Name"
+                        placeholder="Full Name"
+                        css={{
+                           textAlign: "center",
+                           width: "70vw",
+                           margin: "13px",
+                           background: "transparent",
+                           color: "white",
+                           borderStyle: "groove",
+                           borderColor: "#cb1741",
+                           borderLeft: "0px",
+                           borderRight: "0px",
+                           borderTop: "0px",
+                           borderRadius: "9px",
+                        }}
+                        required
+                     />
+                     <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        aria-label="Email"
+                        placeholder="email@domain.com"
+                        css={{
+                           textAlign: "center",
+                           width: "70vw",
+                           margin: "13px",
+                           background: "transparent",
+                           color: "white",
+                           borderStyle: "groove",
+                           borderColor: "#cb1741",
+                           borderLeft: "0px",
+                           borderRight: "0px",
+                           borderTop: "0px",
+                           borderRadius: "9px",
+                        }}
+                        required
+                     />
+                     <input
+                        type="text"
+                        name="phone"
+                        id="phone"
+                        aria-label="Phone Number"
+                        placeholder="Phone (### ### ####)"
+                        css={{
+                           textAlign: "center",
+                           width: "70vw",
+                           margin: "13px",
+                           background: "transparent",
+                           color: "white",
+                           borderStyle: "groove",
+                           borderColor: "#cb1741",
+                           borderLeft: "0px",
+                           borderRight: "0px",
+                           borderTop: "0px",
+                           borderRadius: "9px",
+                        }}
+                     />
+                     <textarea
+                        name="message"
+                        id="message"
+                        rows="4"
+                        aria-label="Message"
+                        placeholder="Type Your Message Here"
+                        required
+                        css={{
+                           textAlign: "center",
+                           width: "70vw",
+                           margin: "13px",
+                           background: "transparent",
+                           color: "white",
+                           borderStyle: "groove",
+                           borderColor: "#cb1741",
+                           borderRadius: "9px",
+                        }}
+                     />
+                     <input
+                        type="submit"
+                        value="Send Message"
+                        css={{
+                           margin: "0 20%",
+                           borderRadius: "13px",
+                           background: "rgba(0, 0, 0, 0.4)",
+                           color: "white",
+                           ":active": {
+                              backgroundColor: "#BE0405",
+                           },
+                        }}
+                     />
+                  </form>
+               </div>
             </div>
          </StyledInnerWrapper>
       </BackgroundImage>
