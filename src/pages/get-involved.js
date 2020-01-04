c0eeb2c74cccd6fa1b7b2294f52b9b04a01c42ea
gabrielawest <50155967+gabrielawest@users.noreply.github.com>
2020-01-03 20:48:24 -0500

    removed partners page, added Get Involved page

---
+++ src/pages/get-involved.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-dupe-keys */
+import React from "react"
+//import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+
+
+const GetInvolved = () => (
+   <Layout>
+      <SEO title="Get Involved!" />
+      <h1 style={{ padding:"16px" }}>Quote Unquote is proudly partnered with:</h1>
+      
+   </Layout>
+)
+
+export default GetInvolved
